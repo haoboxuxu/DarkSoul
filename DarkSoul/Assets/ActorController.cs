76659cda4caa73fbe274daae1e1806ad4efefb3e
haoboxuxu <haoboxuxu@icloud.com>
2021-05-29 14:42:56 +0800

    actor run & walk

--- DarkSoul/Assets/ActorController.cs
+++ DarkSoul/Assets/ActorController.cs
@@ -7,22 +7,38 @@ public class ActorController : MonoBehaviour
 
     public GameObject model;
     public PlayerInput pi;
-
+    public float walkSpeed = 1.4f;
+    public float runMultiplier = 2.7f;
 
     [SerializeField]
     private Animator anim;
+    private Rigidbody rigid;
+    private Vector3 movingVec;
 
     // Start is called before the first frame update
     void Awake()
     {
         pi = GetComponent<PlayerInput>();
         anim = model.GetComponent<Animator>();
-
+        rigid = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        anim.SetFloat("forward", pi.Dup);
+        anim.SetFloat("forward", pi.Dmag * (pi.run ? 2.0f : 1.0f));
+        if (pi.Dmag > 0.1f)
+        {
+            model.transform.forward = pi.Dvec;
+        }
+        movingVec = pi.Dmag * model.transform.forward * walkSpeed * (pi.run ? runMultiplier : 1.0f);
+    }
+
+    void FixedUpdate()
+    {
+        //rigid.position += movingVec * Time.fixedDeltaTime;
+        rigid.velocity = new Vector3(movingVec.x, rigid.velocity.y, movingVec.z);
+        
     }
 }
--- DarkSoul/Assets/PlayerInput.cs
+++ DarkSoul/Assets/PlayerInput.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-
+    [Header("===== Key settings =====")]
     public string keyUp = "w";
     public string keyDown = "s";
     public string keyLeft = "a";
     public string keyRight = "d";
 
+    public string keyA;
+    public string keyB;
+    public string keyC;
+    public string keyD;
+
+    [Header("===== Output signals =====")]
     public float Dup;
     public float Dright;
+    public float Dmag;
+    public Vector3 Dvec;
 
+    // 1. pressing signal
+    public bool run;
+    // 2. trigger once signal
+    // 2. double trigger signal
+
+    [Header("===== Others =====")]
     public bool inputEnabled = true;
 
     private float targetDup;
@@ -40,5 +54,10 @@ public class PlayerInput : MonoBehaviour
 
         Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
         Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+
+        Dmag = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
+        Dvec = Dright * transform.right + Dup * transform.forward;
+
+        run = Input.GetKey(keyA);
     }
 }
